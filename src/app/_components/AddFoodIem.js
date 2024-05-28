8150eca8ed007ab7a394d7629f8b1a486c074df6
junaidnawaz <shaikhmohammedjunaidnawaz@gmail.com>
2024-05-28 15:45:23 +0530

    Added Food Item Collection and API

--- src/app/_components/AddFoodIem.js
+++ src/app/_components/AddFoodIem.js
@@ -5,9 +5,39 @@ const AddFoodItem = () => {
   const [price, setPrice] = useState("");
   const [path, setPath] = useState("");
   const [description, setDescription] = useState("");
+  const [error, setError] = useState(false);
 
-  const handleAddFoodItem = () => {
+  const handleAddFoodItem = async () => {
     console.log(name, price, path, description);
+    if (!name || !price || !path || !description) {
+      setError(true);
+      return false;
+    } else {
+      setError(false);
+    }
+
+    const restaurentData = JSON.parse(localStorage.getItem("restaurentUser"));
+    let resto_id;
+    if (restaurentData) {
+      resto_id = restaurentData._id;
+    }
+
+    let response = await fetch("http://localhost:3000/api/restaurant/foods", {
+      method: "POST",
+      body: JSON.stringify({
+        name,
+        price,
+        img_path: path,
+        description,
+        resto_id,
+      }),
+    });
+    response = await response.json();
+    if (response.success) {
+      alert("Successfully added item");
+    } else {
+      alert("Failed to add Item");
+    }
   };
 
   return (
@@ -22,6 +52,9 @@ const AddFoodItem = () => {
             value={name}
             onChange={(e) => setName(e.target.value)}
           ></input>
+          {error && !name && (
+            <span className="input-error">Please Enter Item Name</span>
+          )}
         </div>
         <div className="input-container">
           <input
@@ -31,6 +64,9 @@ const AddFoodItem = () => {
             value={price}
             onChange={(e) => setPrice(e.target.value)}
           ></input>
+          {error && !price && (
+            <span className="input-error">Please Enter Price</span>
+          )}
         </div>
         <div className="input-container">
           <input
@@ -40,6 +76,9 @@ const AddFoodItem = () => {
             value={path}
             onChange={(e) => setPath(e.target.value)}
           ></input>
+          {error && !path && (
+            <span className="input-error">Please Enter Image Path</span>
+          )}
         </div>
         <div className="input-container">
           <input
@@ -49,6 +88,9 @@ const AddFoodItem = () => {
             value={description}
             onChange={(e) => setDescription(e.target.value)}
           ></input>
+          {error && !description && (
+            <span className="input-error">Please Enter Description</span>
+          )}
         </div>
         <div className="input-container">
           <button className="button" onClick={handleAddFoodItem}>
